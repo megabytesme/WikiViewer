e27340f32384068a3cd51cee8007e92c99afdcff
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-08-20 12:48:05 +0100

    Added "blank" UWP project targeting 1703

---
+++ 1703 UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace _1703_UWP
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
