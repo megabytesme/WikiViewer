05f2909f0b3fcc2ac61b7ce9b00506ece9e6d6d4
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-08-21 12:34:53 +0100

    Improved animations to scroll in either direction based on the viewport size and image aspect ratio, added random durations

--- WikiViewer.Shared.Uwp/Helpers/AnimatedImageLoader.cs
+++ WikiViewer.Shared.Uwp/Helpers/AnimatedImageLoader.cs
@@ -1,3 +1,4 @@
+using System;
 using WikiViewer.Shared.Uwp.Converters;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -57,20 +58,78 @@ namespace WikiViewer.Shared.Uwp.Helpers
 
         private static string GetAnimatedImageHtml(string dataUri)
         {
+            var random = new Random();
+            int duration = random.Next(10, 25);
+
             return $@"
-                <!DOCTYPE html><html><head><style>
-                body {{ margin: 0; overflow: hidden; background-color: transparent; }}
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <style>
+                .container {{
+                    width: 100vw;
+                    height: 100vh;
+                    overflow: hidden;
+                    position: relative;
+                    background-color: transparent;
+                }}
+
+                html, body {{margin: 0;
+                    padding: 0;
+                    width: 100%;
+                    height: 100%;
+                    overflow: hidden;
+                }}
+
                 img {{
-                    position: absolute; left: 50%; top: 50%; height: 100%; width: auto;
-                    min-width: 100%; min-height: 100%; object-fit: cover;
-                    transform: translate(-50%, -50%);
-                    animation: pan 30s infinite alternate ease-in-out;
+                    position: absolute;
                 }}
-                @keyframes pan {{
-                    0% {{ transform: translate(-45%, -50%); }}
-                    100% {{ transform: translate(-55%, -50%); }}
+
+                img.pan-horizontal {{
+                    height: 100%;
+                    width: auto;
+                    animation: pan-horizontal {duration}s infinite alternate ease-in-out;
                 }}
-                </style></head><body><img src='{dataUri}'></body></html>";
+
+                img.pan-vertical {{
+                    width: 100%;
+                    height: auto;
+                    animation: pan-vertical {duration}s infinite alternate ease-in-out;
+                }}
+
+                @keyframes pan-horizontal {{
+                    from {{ left: 0; transform: translate(0, -50%); top: 50%; }}
+                    to   {{ left: 100%; transform: translate(-100%, -50%); top: 50%; }}
+                }}
+
+                @keyframes pan-vertical {{
+                    from {{ top: 0; transform: translate(-50%, 0); left: 50%; }}
+                    to   {{ top: 100%; transform: translate(-50%, -100%); left: 50%; }}
+                }}
+            </style>
+        </head>
+        <body>
+            <div class=""container"">
+                <img id='mainImage' src='{dataUri}'>
+            </div>
+            <script>
+                const img = document.getElementById('mainImage');
+                img.onload = function() {{
+                    const imageAspectRatio = img.naturalWidth / img.naturalHeight;
+                    const containerAspectRatio = document.body.clientWidth / document.body.clientHeight;
+
+                    if (imageAspectRatio < containerAspectRatio) {{
+                        img.classList.add('pan-vertical');
+                    }} else {{
+                        img.classList.add('pan-horizontal');
+                    }}
+                }};
+                if (img.complete) {{
+                    img.onload();
+                }}
+            </script>
+        </body>
+        </html>";
         }
     }
 }
